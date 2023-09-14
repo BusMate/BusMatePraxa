b97ca942b01279d25b9e7f754c14993b83f4cf46
BusMate <busmate978@politehnickasu.edu.rs>
2023-09-14 18:38:59 +0200

    mukodik

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const browserSync = require('browser-sync');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browser = require('browser-sync').create();
@@ -5,9 +6,23 @@ const browser = require('browser-sync').create();
 function style() {
     return gulp.src('./scss/**/*.scss')
 
-    .pipe(sass())
+    .pipe(sass().on('error', sass.logError))
 
     .pipe(gulp.dest('./css'))
+
+    .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./libraries/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
+exports.watch = watch;
